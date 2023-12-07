4e362b3c62e516f023429b464a3ba35165faf217
Alinco8W <ali0207k@gmail.com>
2023-12-07 22:13:40 +0900

    CORS OPTIONS

--- src/database/server.ts
+++ src/database/server.ts
@@ -8,10 +8,16 @@ const col = db.collection('main');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((_req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    if ('OPTIONS' === req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
 
     next();
 });
